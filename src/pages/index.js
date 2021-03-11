49beaa66063bc5d9ec6c944eb01ddde01c30a587
mcoronel2021pa <mcoronel@paloalto.com.ec>
2021-03-11 15:35:33 -0500

    COMMIT DE PRUEBA - MANUEL CORONEL

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,6 +2,9 @@ import React from "react"
 import { Link } from "gatsby"
 import Layout from "../components/layout"
 // import Image from "../components/image"
+
+// --- PRIMER COMENTARIO MC
+
 import SEO from "../components/seo"
 import Login  from "../components/login"
 const IndexPage = () => (
