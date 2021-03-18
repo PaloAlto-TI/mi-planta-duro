1967fea80c6651699219b7c771120ac03e15a950
unknown <jcubero@paloalto.com.ec>
2021-03-18 15:32:28 -0500

    PRUEBA DE FUEGO PRUEBAMERGE2

---
+++ src/pages/home_pruebamerge2.js
@@ -0,0 +1,19 @@
+import React from "react"
+import SEO from "../components/seo"
+import Layout from "../components/layout"
+
+const SecondPage = ({ location }) => {
+
+    if (location.state) {
+      return(
+        <Layout>
+          <SEO title="Page two" />
+          
+          {/* Prueba 1 */}
+      
+      </Layout>)
+    }
+}
+
+    export default SecondPage
+ 
