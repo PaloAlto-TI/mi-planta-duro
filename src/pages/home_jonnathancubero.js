f4eefb0f6ee3f84bc3bbe9258d4e81713e66ede0
unknown <jcubero@paloalto.com.ec>
2021-03-18 15:57:36 -0500

    AGREGA CAMBIO HOME_JONNATHANCUBERO.JS

--- src/pages/home_jonnathancubero.js
+++ src/pages/home_jonnathancubero.js
@@ -5,8 +5,7 @@ import Image from "../components/image"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 
-// ARCHIVO DE MANUEL CORONEL
-// el CAMBIO PUBLIC ELIMINADO
+// MI CAMBIO JONNATHAN CUBERO
 
 const SecondPage = ({ location }) => {
 
